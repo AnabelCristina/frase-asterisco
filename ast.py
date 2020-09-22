7e83cc6864a6c701da6079a5bd9ed16e5e2c2dba
Anabel Cristina <48037781+AnabelCristina@users.noreply.github.com>
2020-09-21 21:49:01 -0300

    Add files via upload

---
+++ ast.py
@@ -0,0 +1,31 @@
+frase = input ("escreva a frase a ser transcrita \n")
+frase_copy = frase
+n = 0
+p = 1
+
+
+
+while (p == 1):    
+    
+    carac = input ("transcrever para * ou x?\n") 
+
+    if (carac == "*" or carac == "x"):
+        p = -44
+        
+    else:
+        print("entrada nao valida")
+        p = 1
+        
+while (p == -44):
+    while (n < len(frase)):
+        if (frase[n] != ' '):
+            frase_copy = frase_copy.replace(frase[n], carac)
+        n = n + 1
+
+    print (frase_copy)
+    p = 2
+
+    
+    
+
+
